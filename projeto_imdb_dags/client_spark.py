2636c8a0169c56b3ba4df8a97b3e9b756d8f6beb
Daniel Cirino <daniel.cirino.02@gmail.com>
2023-07-17 23:56:35 -0300

    Configuração da infra para trabalhar com cluster spark

--- projeto_imdb_dags/client_spark.py
+++ projeto_imdb_dags/client_spark.py
@@ -11,9 +11,21 @@ os.environ["PYSPARK_PYTHON"] = PYTHON_LOCATION
 logging.info(f"PYTHON_LOCATION--->{PYTHON_LOCATION}")
 
 
+def obterJars():
+    dir = "/opt/airflow/spark-jars"
+    jars = os.listdir(dir)
+    stringJars = ""
+
+    for jar in jars:
+        logging.info(f"/opt/airflow/spark-jars/{jar}")
+        stringJars += f"/opt/airflow/spark-jars/{jar},"
+
+    return stringJars[:-1]
+
+
 conf = pyspark.SparkConf()
 conf.setAppName('projeto-imdb')
-conf.setMaster("local[*]")
+conf.setMaster("spark://spark-master:7077")
 
 conf.set("spark.hadoop.fs.s3a.endpoint", Variable.get("AWS_ENDPOINT")) \
     .set("spark.hadoop.fs.s3a.endpoint.region", Variable.get("AWS_REGION")) \
@@ -22,15 +34,15 @@ conf.set("spark.hadoop.fs.s3a.endpoint", Variable.get("AWS_ENDPOINT")) \
     .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .set("spark.hadoop.fs.s3a.connection.ssl.enabled", False) \
     .set("spark.hadoop.com.amazonaws.services.s3.enableV2", True) \
+    .set("spark.jars", obterJars())\
     .set("spark.hadoop.fs.s3a.committer.staging.conflict-mode", "replace") \
     .set("spark.hadoop.fs.s3a.fast.upload", True) \
     .set("spark.hadoop.fs.s3a.path.style.access", True) \
-    .set("spark.hadoop.fs.s3a.committer.name", "magic") \
     .set("spark.sql.sources.commitProtocolClass",
          "org.apache.spark.sql.execution.datasources.SQLHadoopMapReduceCommitProtocol") \
-    .set("parquet.enable.summary-metadata", False) \
-    .set("mapreduce.fileoutputcommitter.marksuccessfuljobs", False)
-
+    # .set("parquet.enable.summary-metadata", False) \
+    # .set("mapreduce.fileoutputcommitter.marksuccessfuljobs", False) \
+    # .set("spark.hadoop.fs.s3a.committer.name", "magic") \
 
 sc = pyspark.SparkContext(conf=conf).getOrCreate()
 
